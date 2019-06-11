69c2be130ad5ce6bbc1ff618c6514af6e00fc423
linhart9 <elinhart9@gmail.com>
2019-06-11 11:17:32 -0700

    Initial Git Upload

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+// Compile sass into CSS & auto-inject into browsers
+function style() {
+    // 1. Where is my scss file
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/**/*.scss'])
+    // 2. Pass that file through sass compiler
+        .pipe(sass())
+    // 3. Where do I save the complied CSS?
+        .pipe(gulp.dest('src/css'))
+    // 4. Stream changes to all browsers
+        .pipe(browserSync.stream())
+}
+
+function js() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server:{
+            baseDir: 'src/'
+        }
+    });
+    // Looking for any change to any scss file
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/**/*.scss'], style);
+    gulp.watch(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js','src/js/**/*.js'], js);
+    gulp.watch('src/*.html').on('change', browserSync.reload);
+};
+
+
+exports.default = gulp.series(style, js, watch)
